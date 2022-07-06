ccc9063fbf1872e88aba69fa7cc2be4e26c8f071
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-07-06 15:18:49 +0900

    Add:Templateモデルの作成

---
+++ app/models/template.rb
@@ -0,0 +1,3 @@
+class Template < ApplicationRecord
+  has_many :letters
+end
---
+++ db/migrate/20220706055907_create_templates.rb
@@ -0,0 +1,10 @@
+class CreateTemplates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :templates do |t|
+      t.string :name, null: false
+      t.string :content, null: false
+
+      t.timestamps
+    end
+  end
+end
