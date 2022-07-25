041c11faac6e5b6d93267c8237a9e794c18b6552
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-07-25 19:11:01 +0900

    Add:CSS、ヘッダー、フッターを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,24 @@
 class UsersController < ApplicationController
-
   require 'net/http'
   require 'uri'
 
-  def new; end
+  def new
+    render layout: 'login'
+  end
 
   def create
     id_token = params[:idToken]
     channel_id = ENV['CHANNEL_ID']
-    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), {'id_token'=>id_token, 'client_id'=>channel_id})
-    line_user_id = JSON.parse(res.body)["sub"]
+    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), {'id_token' => id_token, 'client_id' => channel_id})
+    line_user_id = JSON.parse(res.body)['sub']
     user = User.find_by(line_user_id: line_user_id)
     if user.nil?
       user = User.create(line_user_id: line_user_id)
       session[:user_id] = user.id
-      render :json => user
+      render json: user
     elsif user
       session[:user_id] = user.id
-      render :json => user
+      render json: user
     end
   end
-
 end
