40f55c651ec9f4ea0779c75eb5b8758a73b2b444
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-08-14 12:37:30 +0900

    Add:CarrierWaveの設定を追加

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,25 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    original_filename if original_filename
+  end
+
+  process resize_to_fill: [350, 350, "Center"]
+end
