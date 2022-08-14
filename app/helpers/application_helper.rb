96f381e41bcb97c79131e8df20af85cb9d30e55a
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-08-14 20:27:37 +0900

    Add:meta-tagsの設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'FUTURE LETTER',
+      title: 'FUTURE LETTER',
+      reverse: true,
+      separator: '|',
+      description: 'サプライズでお手紙を送るサービスです。',
+      keywords: '手紙',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon_180.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'FUTURE LETTER',
+        title: 'FUTURE LETTER',
+        description: 'サプライズでお手紙を送るサービスです。',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp_630.jpg'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@FUTURE_LETTER_',
+      }
+    }
+  end
 end
