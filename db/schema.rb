e3bdf12fad8745b6c29348defa30665e2c298515
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-09-06 20:18:19 +0900

    Add:roleカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_20_173334) do
+ActiveRecord::Schema.define(version: 2022_09_06_091429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_08_20_173334) do
     t.string "line_user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
   end
 
   add_foreign_key "anniversaries", "users"
