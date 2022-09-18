1b2fca0e3d73c1a05128669f0b9514be4ca8d979
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-09-19 01:04:11 +0900

    Add:管理画面に関するテストを追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,5 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :chrome
-    # ブラウザを非表示にする場合
-    # driven_by :selenium, using: :headless_chrome
+    driven_by :selenium, using: :headless_chrome
   end
 end
