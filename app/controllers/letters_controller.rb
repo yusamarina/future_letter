4efc84e13ecdb5f22d413e274a2100ce3caf1850
yusamarina <94360716+yusamarina@users.noreply.github.com>
2022-10-18 11:32:27 +0900

    Add:reserveアクションをサービスクラスへ移植

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -18,75 +18,8 @@ class LettersController < ApplicationController
     letter = Letter.find(params[:id])
     letter.save!(validate: false)
     render json: letter
-    if letter.user == current_user
-      message = {
-        "type": 'text',
-        "text": "お手紙が登録されました！\nお届けまでお楽しみに$$",
-        "emojis": [
-          {
-            "index": 23,
-            "productId": "5ac22bad031a6752fb806d67",
-            "emojiId": "070"
-          },
-          {
-            "index": 24,
-            "productId": "5ac22bad031a6752fb806d67",
-            "emojiId": "189"
-          }
-        ]
-      }
-      client = Line::Bot::Client.new { |config|
-        config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-      }
-      response = client.push_message(current_user.line_user_id, message)
-      p response
-    else
-      message = {
-        "type": 'text',
-        "text": "お手紙の宛先が確認されました$\n設定日時に相手へお手紙が届きます！お楽しみに$",
-        "emojis": [
-          {
-            "index": 14,
-            "productId": "5ac22bad031a6752fb806d67",
-            "emojiId": "186"
-          },
-          {
-            "index": 38,
-            "productId": "5ac22bad031a6752fb806d67",
-            "emojiId": "143"
-          }
-        ]
-      }
-      client = Line::Bot::Client.new { |config|
-        config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-      }
-      response = client.push_message(letter.user.line_user_id, message)
-      p response
-      message = {
-        "type": 'text',
-        "text": "お手紙はサプライズで届きます！\nお届けまでお楽しみに$$",
-        "emojis": [
-          {
-            "index": 26,
-            "productId": "5ac22bad031a6752fb806d67",
-            "emojiId": "070"
-          },
-          {
-            "index": 27,
-            "productId": "5ac22bad031a6752fb806d67",
-            "emojiId": "189"
-          }
-        ]
-      }
-      client = Line::Bot::Client.new { |config|
-        config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-      }
-      response = client.push_message(current_user.line_user_id, message)
-      p response
-    end
+    register_letter = RegisterLetterService.new(letter, current_user)
+    register_letter.call
   end
 
   def new
@@ -109,28 +42,6 @@ class LettersController < ApplicationController
   def message
     push_message = PushMessageService.new(current_user)
     push_message.call
-    # message = {
-    #   "type": 'text',
-    #   "text": "お手紙の宛先確認メッセージを送りました$\n送信が許可されるまでお待ちください！$",
-    #   "emojis": [
-    #     {
-    #       "index": 19,
-    #       "productId": "5ac2197b040ab15980c9b43d",
-    #       "emojiId": "027"
-    #     },
-    #     {
-    #       "index": 39,
-    #       "productId": "5ac2173d031a6752fb806d56",
-    #       "emojiId": "202"
-    #     }
-    #   ]
-    # }
-    # client = Line::Bot::Client.new { |config|
-    #   config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-    #   config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-    # }
-    # response = client.push_message(current_user.line_user_id, message)
-    # p response
   end
 
   def edit
